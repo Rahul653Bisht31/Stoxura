231c9f8b9201c5d2c4bedd9a23ff824456d86829
Rahul Bisht <bishtrahul653@gmail.com>
2025-09-02 19:43:59 +0530

    Added Products component

--- src/landing_page/products/ProductsPage.js
+++ src/landing_page/products/ProductsPage.js
@@ -4,16 +4,53 @@ import LeftSection from "./LeftSection";
 import RightSection from "./RightSection";
 import Universe from "./Universe";
 
-
-function ProductsPage(){
-    return(
-        <>
-            <Hero />
-            <LeftSection />
-            <RightSection />
-            <Universe />
-        </>
-    );
+function ProductsPage() {
+  return (
+    <>
+      <Hero />
+      <LeftSection
+        imageURL="media/images/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegent UI, and more.Enjoy the kite expreience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection 
+        imageURL="media/images/console.png"
+        productName="Console"
+        productDescription="The central dashboard for your Stoxura account. Gain insights into your trades and investments with in-depth reports and visualisations."
+        learnMore=""
+      />
+      <LeftSection
+        imageURL="media/images/coin.png"
+        productName="Coin"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investement experience on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection
+        imageURL="media/images/kiteconnect.png"
+        productName="Kite Connect API"
+        productDescription="Build powerful trading platforms and experiences with out super simple HTTPS/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
+        learnMore=""
+      />
+      <LeftSection
+        imageURL="media/images/varsity.png"
+        productName="Varsity mobile"
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <p className="text-center mt-5">Want to know more about our technology stack? Check out the Stoxura.tech blog.</p>
+      <Universe />
+    </>
+  );
 }
 
 export default ProductsPage;
--- src/landing_page/products/Universe.js
+++ src/landing_page/products/Universe.js
@@ -2,7 +2,37 @@ import React from "react";
 
 function Universe(){
     return(
-        <h1>Universe</h1>
+        <div className="container mt-5 mb-4">
+            <div className="row text-center">
+                <h1>The Stoxura Universe</h1>
+                <p>Extend your trading and investement experience even further with our partner platforms</p>
+                <div className="col-4 p-3 mt-5">
+                    <img src="media\images\smallcaseLogo.png" alt="" />
+                    <p className="text-small text-muted">Thematic investement platform</p>
+                </div>
+                <div className="col-4 p-3 mt-5">
+                    <img src="media\images\streakLogo.png" style={{width:"38%"}} alt="" />
+                    <p className="text-small text-muted">Algo & strategy platform</p>
+                </div>
+                <div className="col-4 p-3 mt-5">
+                    <img src="media\images\sensibullLogo.svg" style={{width:"50%"}} alt="" />
+                    <p className="text-small text-muted">Options trading platform</p>
+                </div>
+                <div className="col-4 p-2 mt-4">
+                    <img src="media\images\stoxuraLogo.png" style={{width:"50%"}} alt="" />
+                    <p className="text-small text-muted">Asset management</p>
+                </div>
+                <div className="col-4 p-3 mt-5">
+                    <img src="media\images\goldenpiLogo.png" alt="" />
+                    <p className="text-small text-muted mt-2">Bonds trading platform</p>
+                </div>
+                <div className="col-4 p-3 mt-5">
+                    <img src="media\images\dittoLogo.png" style={{width:"25%"}} alt="" />
+                    <p className="text-small text-muted mt-2">Options trading platform</p>
+                </div>
+                <button className="p-2 btn btn-primary fs-5 mb-5 mt-5" style={{width:"15%",margin:"0 auto"}}>Sign up now</button>
+            </div>
+        </div>
     );
 }
 
